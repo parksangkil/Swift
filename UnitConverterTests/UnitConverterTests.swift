3df94b95498f80d0b518b89bd9bbcfc5a7dfee8d
netive <netive@netiveui-MacBook-Pro.local>
2015-05-23 20:28:44 +0900

    Initial Commit

---
+++ UnitConverterTests/UnitConverterTests.swift
@@ -0,0 +1,36 @@
+//
+//  UnitConverterTests.swift
+//  UnitConverterTests
+//
+//  Created by netive on 2015. 5. 23..
+//  Copyright (c) 2015년 netive. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class UnitConverterTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
