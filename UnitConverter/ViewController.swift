5f85cc5e6a340df85b6d884f9e71e98ad3c28c77
netive <netive@netiveui-MacBook-Pro.local>
2015-05-24 15:39:13 +0900

    Test

--- UnitConverter/ViewController.swift
+++ UnitConverter/ViewController.swift
@@ -13,13 +13,34 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
+    @IBAction func textFieldReturn(sender: AnyObject) {
+        sender.resignFirstResponder()
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
+    override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
+        tempText.endEditing(true)
+    }
+    
+    @IBOutlet weak var tempText: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    @IBAction func convertTemp(sender: AnyObject) {
+        let fahrenheit = (tempText.text as NSString).doubleValue
+        let celsius = (fahrenheit - 32) / 1.8
+        let resultText = "celsius \(celsius)"
+        resultLabel.text = resultText
+        
+        
+    }
+    
+    
 }
 
